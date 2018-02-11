a89e5d09d2d5db3bbf3da2866728e6f74a044200
Brian Molina <bmolina@digitalstrategy.com.ec>
2018-02-11 18:24:54 -0500

    Add project files.

---
+++ SendPositionR/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SendPositionR
+{
+    class Program
+    {/// <summary>
+    /// Se debe escribir la url donde se encuentre la pagina que recibirá los datos 
+    /// </summary>
+        public static SignalRCliente signalRCliente = new SignalRCliente("http://localhost:52127");
+
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine("Latitud");
+                float Lat = (float)-0.174391 + i;
+                Console.WriteLine("Longitud");
+                float Lon = (float)-78.48326500000002 + i;
+                Enviar(Lat, Lon, 1, 2, "nestor");
+
+                Thread.Sleep(10000);
+                Console.WriteLine("Latitud");
+                Lat = (float)-0.1717243 + i;
+                Console.WriteLine("Longitud");
+                Lon = (float)-78.48371889999999 + i;
+                Enviar(Lat, Lon, 2, 2, "Brian");
+
+                Thread.Sleep(20000);
+            }
+
+
+
+
+            //Console.WriteLine("Latitud");
+            //Lat = (float)-0.176688;
+            //Console.WriteLine("Longitud");
+            //Lon = (float)-78.4841;
+            //Enviar(Lat, Lon, 2);
+
+            Console.WriteLine("Posicion enviada correctamente");
+            Console.ReadLine();
+        }
+
+
+        private static async Task Enviar(float lat, float lon, int agente, int empresa, string nombre)
+        {
+            await signalRCliente.Start().ContinueWith(task =>
+            {
+                if (task.IsFaulted)
+                    Console.WriteLine("Error", "An error occurred when trying to connect to SignalR: " + task.Exception.InnerExceptions[0].Message);
+            }
+                   );
+            LivePositionRequest lpr = new LivePositionRequest
+            {
+                EmpresaId = empresa,
+                AgenteId = agente,
+                fecha = DateTime.Now,
+                Lat = lat,
+                Lon = lon,
+                Nombre = nombre,
+            };
+            //try to reconnect every 10 seconds, just in case
+
+            signalRCliente.SendMessage(lpr);
+
+
+        }
+
+    }
+}
---
+++ SendPositionR/SignalRCliente.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNet.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SendPositionR
+{
+    public class SignalRCliente : INotifyPropertyChanged
+    {
+        private HubConnection Connection;
+        private IHubProxy ChatHubProxy;
+
+        public delegate void MessageReceived(string username, string message);
+        public event MessageReceived OnMessageReceived;
+
+        public SignalRCliente(string url)
+        {
+            Connection = new HubConnection(url);
+
+            Connection.StateChanged += (StateChange obj) => {
+                OnPropertyChanged("ConnectionState");
+            };
+
+            ChatHubProxy = Connection.CreateHubProxy("recived");
+            ChatHubProxy.On<string, string>("MessageReceived", (username, text) => {
+                OnMessageReceived?.Invoke(username, text);
+            });
+        }
+
+        public void SendMessage(LivePositionRequest livePosition)
+        {
+            ChatHubProxy.Invoke("SendPosition", livePosition);
+        }
+
+        public Task Start()
+        {
+            return Connection.Start();
+        }
+
+        public bool IsConnectedOrConnecting
+        {
+            get
+            {
+                return Connection.State != ConnectionState.Disconnected;
+            }
+        }
+
+        public ConnectionState ConnectionState { get { return Connection.State; } }
+
+        public static async Task<SignalRCliente> CreateAndStart(string url)
+        {
+            var client = new SignalRCliente(url);
+            await client.Start();
+            return client;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+
+}
